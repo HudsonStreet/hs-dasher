9b3eb48794f99c059e07b2c57c7edb108c83c1ee
jinsiang2008 <jinsiang2008@gmail.com>
2019-05-13 00:54:39 -0400

    testing

--- main.py
+++ main.py
@@ -1,69 +1,108 @@
-# import dash
-# from dash.dependencies import Input, Output
-# import dash_core_components as dcc
-# import dash_html_components as html
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
 
 import pandas as pd
-# from datetime import datetime as dt
-# import plotly.graph_objs as graob
+from datetime import datetime as dt
+import plotly.graph_objs as graob
 
 ##Load vix data
-# headers = ['date_time', 'vix_value']
-# dtypes = {'date_time':'str', 'vix_value': 'float'}
-# parse_dates = ['date_time']
-# VIX_DATA_FORM = pd.read_csv("../hs-vix-calculator/vix_w_datetime.csv", header=None, names=headers, dtype=dtypes, parse_dates=parse_dates)
+headers = ['date_time', 'vix_value']
+dtypes = {'date_time':'str', 'vix_value': 'str'}
+parse_dates = ['date_time']
+df = pd.read_csv("../VIX_Calculator/vix_w_datetime.csv", header=None, names=headers, dtype=dtypes, parse_dates=parse_dates)
+df = df.dropna()
+df['date_time'] = pd.to_datetime(df.date_time, infer_datetime_format=True)
+df['Month'] = df['date_time'].dt.month
+df['Day'] = df['date_time'].dt.day
+#df['TimeOfDate'] = df['date_time'].dt.time()
+df['Hour'] = df['date_time'].dt.hour
+df['Minute'] = df['date_time'].dt.minute
 
-# app = dash.Dash(__name__)
+print(df['date_time'])
+print(df['vix_value'])
+print(df['Hour'])
+print(df['Minute'])
+#print(df['TimeOfDate'])
 
-# app.layout = html.Div([
-#    html.Div([
-#         html.H1("HS Options VIX"),
-#     ], style={
-#         'textAlign': "center"}),
+app = dash.Dash(__name__)
 
-#     dcc.Graph(id='options_vix_graph'),
+app.layout = html.Div([
+   html.Div([
+        html.H1("HS Options VIX"),
+    ], style={
+        'textAlign': "center"}),
 
-#     html.Div([
-#         dcc.Slider(
-#         id='time_slider',
-#         min=0,
-#         max=5,
-#         marks={
-#             0: '1 day', 
-#             1: '5 days',
-#             2: '1 month',
-#             3: '3 months',
-#             4: '6 months',
-#             5: 'YTD'
-#         },
-#         value=1,
-#     )
-#     ])
+    dcc.Graph(id='options_vix_graph'),
+    # dcc.Interval(id='refresh', Interval=100)
+
+    # html.Div([
+    #     dcc.Slider(
+    #     id='time_slider',
+    #     min=0,
+    #     max=5,
+    #     marks={
+    #         0: '1 day', 
+    #         1: '5 days',
+    #         2: '1 month',
+    #         3: '3 months',
+    #         4: '6 months',
+    #         5: 'YTD'
+    #     },
+    #     value=1,
+    # )
+    # ])    
+], className="container")
+
+@app.callback(
+    Output('options_vix_graph', 'figure')
+    #events=[Event('refresh', 'interval')]
+    # [Input('time_slider','value')] 
+)
+def update_graph():
+    trace = []
+
+    trace.append(graob.Scatter(
+        x = df["Month"],
+        y = df["vix_value"],
+        mode='lines',
+        opacity=0.7,
+        name=f'VIX',
+        textposition='bottom center',
+    ))
+
+    # traces = [trace1]
+    # data = [val for sublist in traces for val in sublist]
     
-# ], className="container")
-
-# @app.callback(
-#     Output('options_vix_graph', 'figure'), 
-#     [Input('time_slider','value')] 
-# )
-# def update_graph(selected_slider_value):
-#     trace = []
-
-#     return {
-#         'data': [{
-#             'x': df.index,
-#             'y': df.Close
-#         }],
-#         'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
-#     }
+    return {
+        'data': trace,
+        'layout': graob.Layout(
+            colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+            height=600,
+            title=f"Options vix",
+            xaxis={"title":"Date",
+            #     'rangeselector': {'buttons': list([
+            #     {'count': 1, 'label': '1M', 'step': 'month', 'stepmode': 'backward'},
+            #     {'count': 6, 'label': '6M', 'step': 'month', 'stepmode': 'backward'},
+            #     {'step': 'all'}
+            # ])}, 'rangeslider': {'visible': True}, 'type': 'date'},
+            },
+            yaxis={
+                "title":"VIX",
+            }
+        )
+    }
 
 # app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
-    # app.run_server()
-    headers = ['date_time', 'vix_value']
-    dtypes = {'date_time':'str', 'vix_value': 'str'}
-    parse_dates = ['date_time']
-    VIX_DATA_FORM = pd.read_csv("../hs-vix-calculator/vix_w_datetime.csv", header=None, names=headers, dtype=dtypes, parse_dates=parse_dates)
+    app.run_server(debug=True)
+    
+    #For testing
+    # headers = ['date_time', 'vix_value']
+    # dtypes = {'date_time':'str', 'vix_value': 'str'}
+    # parse_dates = ['date_time']
+    # VIX_DATA_FORM = pd.read_csv("../VIX_Calculator/vix_w_datetime.csv", header=None, names=headers, dtype=dtypes, parse_dates=parse_dates)
 
-    print(VIX_DATA_FORM)
+    # print(VIX_DATA_FORM)
